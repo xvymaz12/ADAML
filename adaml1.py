703d40d3a50548f84f1e20a07fee60b64fbee0d3
xvymaz12 <xvymaz12@stud.fit.vutbr.cz>
2024-09-15 18:36:43 +0300

    Added code comments

--- adaml1.py
+++ adaml1.py
@@ -1,49 +1,40 @@
-#Authors: Emma Hirvonen, Helmi Toropainen, Jan Vymazal
-#Course: Advanced Data Analysis and Machine Learning
-#Topic: Wind Turbines Failure (B)
-
+# BM20A6100 Project work wind turbine failure
+# Data summary and visualisation
+# Emma Hirvonen, Helmi Toropainen, Jan Vymazal
 
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 
-
+# load data for all turbines
 WT2 = pd.read_excel('/content/sample_data/data.xlsx', 'No.2WT')
 WT39 = pd.read_excel('/content/sample_data/data.xlsx', 'No.39WT')
 WT14 = pd.read_excel('/content/sample_data/data.xlsx', 'No.14WT')
 WT3 = pd.read_excel('/content/sample_data/data.xlsx', 'No.3')
 
-WT2 = WT2.iloc[:, :-1] # WT2 has extra variable at the end
-
-
+# check for missing data
 turbines = [WT2, WT39, WT14, WT3]
 for turbine in turbines:
   missing_values = turbine.isnull().sum()
   record_count = len(turbine)
   means = turbine.mean()
-  print(f"Number of missing values {missing_values}")
+  print(f"Number of missing values{missing_values}")
   print(f"Number of records {record_count}")
   print(means)
 
+import matplotlib.pyplot as plt
 
+# correlations between variables for turbine 2
 corr_matrix_2 = WT2.corr()
 plt.imshow(corr_matrix_2, cmap='Blues')
+
+# correlations between variables for turbine 39
 corr_matrix_39 = WT39.corr()
 plt.imshow(corr_matrix_39, cmap='Blues')
 
-corr2 = WT39.corrwith(WT2)
-print(corr2)
-
-X = pd.concat([WT2, WT39], axis=1);
-corr_matrix_both = X.corr()
-plt.imshow(corr_matrix_both, cmap='Blues')
-
-WT2 = pd.read_excel('/content/sample_data/data.xlsx', 'No.2WT')
-WT39 = pd.read_excel('/content/sample_data/data.xlsx', 'No.39WT')
+# boxplots for all variables
 for column in WT39.columns:
   plt.figure()
   plt.boxplot([WT2[column],WT39[column]], labels=["WT2", "WT39"])
-  plt.title(f'Boxplot of {column}')
+  plt.title(f'Boxplot of attribute {column}')
   plt.grid(True)
   plt.show()
 
@@ -55,6 +46,7 @@ plt.show()
 
 # chosen variables to look into: 5, 11, 12, 15, 18, 27
 
+# plot time series data for chosen variables
 picked_columns = [5, 11, 12, 15, 18, 27]
 for col in picked_columns:
   plt.figure()
@@ -64,7 +56,20 @@ for col in picked_columns:
   plt.xlabel("Time")
   plt.legend()
   plt.grid(True)
-  
+
+# plot time series data for all variables for further inspection
+picked_columns = range(27)
+for col in picked_columns:
+  plt.figure()
+  plt.plot(WT2.iloc[:, col], label="WT2", color='blue')
+  plt.plot(WT39.iloc[:, col], label="WT39", color='green')
+  plt.title(f"Time series of attribute {col+1}")
+  plt.xlabel("Time")
+  plt.legend()
+  plt.grid(True)
+  plt.show()
+
+# plot variable 28 from WT2 with variable 27 from WT39
 plt.figure()
 plt.plot(WT2.iloc[:, 27], label="WT2", color='blue')
 plt.plot(WT39.iloc[:, 26], label="WT39", color='green')
@@ -74,6 +79,7 @@ plt.legend()
 plt.grid(True)
 plt.show()
 
+# plot variable 27 from WT2 with variable 26 from WT39
 plt.figure()
 plt.plot(WT2.iloc[:, 26], label="WT2", color='blue')
 plt.plot(WT39.iloc[:, 25], label="WT39", color='green')
